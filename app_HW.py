76a51ea477c027813fa0a8c289112d75f04d2066
Honorine Kayitengerwa <honorinekayitengerwa@gmail.com>
2023-05-25 08:49:06 -0400

    deleted a file

--- app_HW.py
+++ app_HW.py
@@ -1,23 +1,31 @@
 # Import the dependencies.
 from flask import Flask, jsonify
 
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 
 #################################################
 # Database Setup
 #################################################
-
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
